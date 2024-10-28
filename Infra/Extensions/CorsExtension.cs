128d276c193cb69671befabe9edbcd11b4da9024
Ulisses Parola <ulisses.parola@gmail.com>
2024-10-28 19:29:55 -0300

    CORS

---
+++ Infra/Extensions/CorsExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Extensions;
+
+public static class CorsExtension
+{
+    public static void AddCors(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.WithOrigins(
+                    "https://caderneta-vacinacao-693e63316fd9.herokuapp.com", // Domínio do frontend em produção
+                    "http://localhost:4200"                    // Domínio do frontend em desenvolvimento
+                )
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+    }
+}
